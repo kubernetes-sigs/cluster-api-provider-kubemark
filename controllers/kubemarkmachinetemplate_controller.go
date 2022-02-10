f513c312191c8f57009e3eb3205e646c6235e36c
Michael McCune <elmiko@redhat.com>
2022-02-10 15:42:34 -0500

    add basic controller for KubemarkMachineTemplates
    This change adds a very simple controller that will reconcile on
    KubemarkMachineTemplates. It will log the template names as it
    reconciles them. Also updates the generated files.

---
+++ controllers/kubemarkmachinetemplate_controller.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	infrav1 "github.com/kubernetes-sigs/cluster-api-provider-kubemark/api/v1alpha4"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type KubemarkMachineTemplateReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kubemarkmachinetemplates,verbs=get;list;watch;create;update;patch;delete
+
+func (r *KubemarkMachineTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := r.Log.WithValues("kubemarkmachinetemplate", req.NamespacedName)
+
+	var machineTemplate infrav1.KubemarkMachineTemplate
+	if err := r.Get(ctx, req.NamespacedName, &machineTemplate); err != nil {
+		logger.Error(err, "unable to fetch KubemarkMachineTemplate")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	logger.Info("reconciling KubemarkMachineTemplate", machineTemplate.Name)
+
+	return ctrl.Result{}, nil
+}
+
+func (r *KubemarkMachineTemplateReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&infrav1.KubemarkMachineTemplate{}).
+		Complete(r)
+}
--- main.go
+++ main.go
@@ -83,6 +83,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KubemarkMachine")
 		os.Exit(1)
 	}
+	if err = (&controllers.KubemarkMachineTemplateReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("KubemarkMachineTemplate"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(ctx, mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KubemarkMachineTemplate")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
